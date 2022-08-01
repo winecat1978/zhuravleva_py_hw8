bffc8ddd0119e2748ab37c700c6be892e90b31d6
Denis <den_makarcev@mail.ru>
2022-08-01 21:18:22 +0300

    Все для Вас 2.0!!!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 некой компании \ студентами вуза \ учениками школы
 '''
 database = {}
+db = {'parents': 1, 'children': 2}
 
 
 def reading_file_to_dict(number_file):
@@ -12,15 +13,16 @@ def reading_file_to_dict(number_file):
         database[number_file] = []
         for i in data:
             database[number_file].append(tuple(i.split(';')))
-        print(database)
 
 
-def print_childrens(second_name):
-    pass
+def print_children(second_name):
+    id = [i[0] for i in database[db['parents']] if second_name in i][0]
+    deti = [i for i in database[db['children']] if id == i[1]]
+    print(*[' '.join(i[2:4]) + '\n' for i in deti])
 
 
 reading_file_to_dict(1)
 reading_file_to_dict(2)
 print(database)
-print_childrens('Иванов')
+print_children('Ivanov')
 # Создать решение для вывода детей по фамилии
