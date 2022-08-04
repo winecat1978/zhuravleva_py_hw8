87274595358d425f1a1c62d8b83958c046441740
Denis <den_makarcev@mail.ru>
2022-08-04 20:30:55 +0300

    Все для Вас 3.0!!!

---
+++ logger.py
@@ -0,0 +1,118 @@
+from data_create import name_data, surname_data, phone_data, address_data
+
+
+def input_data():
+    name = name_data()
+    surname = surname_data()
+    phone = phone_data()
+    address = address_data()
+    var = int(input("В каком формате Вы хотите записать данные?\n\n"
+                    "1 Вариант:\n\n"
+                    "Иванов\n"
+                    "Иван\n"
+                    "+79991111213\n"
+                    "г. Москва ул. 1-я Синичкина д3к1\n\n"
+                    "2 Вариант:\n\n"
+                    "Иванов;Иван;+79991111213;г. Москва ул. 1-я Синичкина д3к1\n\n"
+                    "Выберите номер варианта: "))
+
+    while var != 1 and var != 2:
+        print('Ты дурак?! Даю тебе последний шанс')
+        var = int(input("Введите номер варианта: "))
+
+    if var == 1:
+        with open('data_first_variant.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{name}\n{surname}\n{phone}\n{address}\n\n')
+    else:
+        with open('data_second_variant.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{name};{surname};{phone};{address}\n\n')
+
+
+def print_data():
+    print('Вывожу данные для Вас из 1-ого файла\n')
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as file:
+        data_first = file.readlines()
+        data_first_version_second = []
+        data_middle = ''
+        j = 0
+        for i in range(len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_version_second.append(''.join(data_first[j:i + 1]))
+                j = i
+        data_first = data_first_version_second
+        print(''.join(data_first))
+    print('Вывожу данные для Вас из 2-ого файла\n')
+    with open('data_second_variant.csv', 'r', encoding='utf-8') as file:
+        data_second = list(file.readlines())
+        print(*data_second)
+    return data_first, data_second
+
+
+def put_data():
+    print('Из какого файла Вы хотите изменить данные?')
+    data_first, data_second = print_data()
+    number_file = int(input('Введите номер файла: '))
+
+    while number_file != 1 and number_file != 2:
+        print('Ты дурак?! Даю тебе последний шанс')
+        number_file = int(input('Введите номер файла: '))
+
+    if number_file == 1:  # Можно сделать нумерацию внутри файла
+        print("Какую именно запись по счету Вы хотите изменить?")
+        number_journal = int(input('Введите номер записи: '))
+        number_journal -= 1
+        # Можно добавить проверку, чтобы человек не выходил за пределы записей
+        print(f'Изменить данную запись\n{data_first[number_journal]}')
+        name = name_data()
+        surname = surname_data()
+        phone = phone_data()
+        address = address_data()
+        data_first = data_first[:number_journal] + [f'{name}\n{surname}\n{phone}\n{address}\n'] + \
+                     data_first[number_journal + 1:]
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_first))
+        print('Изменения успешно сохранены!')
+    else:
+        print("Какую именно запись по счету Вы хотите изменить?")
+        number_journal = int(input('Введите номер записи: '))
+        number_journal -= 1
+        # Можно добавить проверку, чтобы человек не выходил за пределы записи
+        print(f'Изменить данную запись\n{data_second[number_journal]}')
+        name = name_data()
+        surname = surname_data()
+        phone = phone_data()
+        address = address_data()
+        data_second = data_second[:number_journal] + [f'{name};{surname};{phone};{address}\n'] + \
+                      data_second[number_journal + 1:]
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_second))
+        print('Изменения успешно сохранены!')  # Можно вывести конечные данные
+
+
+def delete_data():
+    print('Из какого файла Вы хотите удалить данные?')
+    data_first, data_second = print_data()
+    number_file = int(input('Введите номер файла: '))
+
+    while number_file != 1 and number_file != 2:
+        print('Ты дурак?! Даю тебе последний шанс')
+        number_file = int(input('Введите номер файла: '))
+
+    if number_file == 1:  # Можно сделать нумерацию внутри файла
+        print("Какую именно запись по счету Вы хотите удалить?")
+        number_journal = int(input('Введите номер записи: '))
+        # Можно добавить проверку, чтобы человек не выходил за пределы записи
+        print(f'Удалить данную запись\n{data_first[number_journal - 1]}')
+        data_first = data_first[:number_journal] + data_first[number_journal + 1:]
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_first))
+        print('Изменения успешно сохранены!')
+    else:
+        print("Какую именно запись по счету Вы хотите удалить?")
+        number_journal = int(input('Введите номер записи: '))
+        # Можно добавить проверку, чтобы человек не выходил за пределы записи
+        print(f'Удалить данную запись\n{data_second[number_journal - 1]}')
+        data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_second))
+        print('Изменения успешно сохранены!')  # Можно вывести конечные данные
--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-'''
-Создать информационную систему позволяющую работать с сотрудниками
-некой компании \ студентами вуза \ учениками школы
-'''
-database = {}
-db = {'parents': 1, 'children': 2}
+from ui import interface
 
 
-def reading_file_to_dict(number_file):
-    with open(f'{number_file}.txt', 'r', encoding='utf-8') as file_1:
-        data = [i.split('\n')[0] for i in file_1.readlines()]
-        # print(data)
-        database[number_file] = []
-        for i in data:
-            database[number_file].append(tuple(i.split(';')))
-
-
-def print_children(second_name):
-    id = [i[0] for i in database[db['parents']] if second_name in i][0]
-    deti = [i for i in database[db['children']] if id == i[1]]
-    print(*[' '.join(i[2:4]) + '\n' for i in deti])
-
-
-reading_file_to_dict(1)
-reading_file_to_dict(2)
-print(database)
-print_children('Ivanov')
-# Создать решение для вывода детей по фамилии
+if __name__ == "__main__":
+    interface()
