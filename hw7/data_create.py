3af8be385cb978eb64cd40ab58a7a9a08eccaa5d
Katherine_20 <mob1911494@gmail.com>
2022-08-07 16:44:16 +0300

    seminar ended

---
+++ hw7/data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите Ваше имя: ')
+    print('Очень красивое имя! (а меня зовут гб_бот, меня создала компания GeekBrains!')
+    return name
+
+
+def surname_data():
+    surname = input('Введите Вашу фамилию: ')
+    return surname
+
+
+def phone_data():
+    phone = input('Введите Ваш телефон: ')
+    return phone
+
+
+def address_data():
+    address = input('Введите Ваш адрес: ')
+    return address
